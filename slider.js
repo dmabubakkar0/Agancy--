3060252545c2c1eefb9087f6aa110203c00d3380
dmabubakkar <dmabubakkar06@gmail.com>
2024-03-11 11:50:35 +0600

    oky

--- slider.js
+++ slider.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $(".hero_slider").owlCarousel({
+  $(".hero_slider ").owlCarousel({
     loop: true,
     margin: 10,
     responsiveClass: true,
@@ -18,6 +18,35 @@ $(document).ready(function () {
       },
       1000: {
         items: 2,
+        nav: false,
+        margin: 30,
+      },
+    },
+  });
+});
+
+
+$(document).ready(function () {
+  $(".testimonial ").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    autoplay: 100,
+    nav: false,
+   center:true,
+    items:3,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      
+      },
+      600: {
+        items: 3,
+        nav: false,
+      },
+      1000: {
+        items: 3,
         nav: false,
         margin: 30,
       },
